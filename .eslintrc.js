8788b83cfb7b905d34c3aeb307e40d52990b99b8
tsu-nera <fox10225fox@gmail.com>
2019-05-04 22:00:31 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-param-reassign': 0,
+    'react/destructuring-assignment': 0,
+    'react/require-default-props': 0
+  }
+};
