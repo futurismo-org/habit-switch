ac10154a1097e95da1076e17ccfb232ca927aa7e
tsu-nera <fox10225fox@gmail.com>
2019-05-04 22:48:05 +0900

    add styled-component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,54 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledCenterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledTimer = styled.span`
+  color: black;
+  font-size: 10vw;
+`;
+
+const StyledTimerButtonContainer = styled.div`
+  margin: 10px;
+  width: 30vw;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const StyledTimerButton = styled.button`
+  font-size: 30px;
+`;
 
 const Home = () => (
-  <div>
-    <div className="center-container">
-      <h2>Titan Habit Tracker</h2>
-      <div>
-        <span className="timer-number">00</span>
-        <span className="timer-semicolon">:</span>
-        <span className="timer-number">00</span>
-        <span className="timer-semicolon">:</span>
-        <span className="timer-number">00</span>
-      </div>
-      <div className="timer-button-container">
-        <button className="timer-button" type="button" name="button">
-          START
-        </button>
-        <button className="timer-button" type="button" name="button">
-          STOP
-        </button>
-        <button className="timer-button" type="button" name="button">
-          RESET
-        </button>
-      </div>
+  <StyledCenterContainer>
+    <h2>Titan Habit Tracker</h2>
+    <div>
+      <StyledTimer>00</StyledTimer>
+      <StyledTimer>:</StyledTimer>
+      <StyledTimer>00</StyledTimer>
+      <StyledTimer>:</StyledTimer>
+      <StyledTimer>00</StyledTimer>
     </div>
-
-    <style jsx>{`
-      .center-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .timer-number,
-      .timer-semicolon {
-        color: black;
-        font-size: 10vw;
-      }
-
-      .timer-button-container {
-        width: 30vw;
-        display: flex;
-        justify-content: space-around;
-      }
-
-      .timer-button {
-        font-size: 30px;
-      }
-    `}</style>
-  </div>
+    <StyledTimerButtonContainer>
+      <StyledTimerButton type="button" name="button">
+        START
+      </StyledTimerButton>
+      <StyledTimerButton type="button" name="button">
+        STOP
+      </StyledTimerButton>
+      <StyledTimerButton type="button" name="button">
+        RESET
+      </StyledTimerButton>
+    </StyledTimerButtonContainer>
+  </StyledCenterContainer>
 );
 
 export default Home;
