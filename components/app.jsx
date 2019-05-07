add2fb2acdf348508511858a216741518ded26a8
tsu-nera <fox10225fox@gmail.com>
2019-05-07 15:01:19 +0900

    add confirm-alart

--- components/app.jsx
+++ components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import styled from 'styled-components';
 
+import { confirmAlert } from 'react-confirm-alert';
 import Timer from './timer';
 import TimerButton from './timer-button';
+
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 const StyledCenterContainer = styled.div`
   display: flex;
@@ -19,20 +21,38 @@ const StyledTimerButtonContainer = styled.div`
   justify-content: space-around;
 `;
 
-const App = props => (
-  <StyledCenterContainer>
-    <h2>Titan Habit Tracker</h2>
-    <Timer
-      days={props.days}
-      hours={props.hours}
-      minutes={props.minutes}
-      seconds={props.seconds}
-    />
-    <StyledTimerButtonContainer>
-      <TimerButton text="START" handleClick={props.startTimer} />
-      <TimerButton text="RESET" handleClick={props.resetTimer} />
-    </StyledTimerButtonContainer>
-  </StyledCenterContainer>
-);
+const App = props => {
+  const confirm = () => {
+    confirmAlert({
+      title: '確認',
+      message: '本当にリセットしますか？',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => props.resetTimer()
+        },
+        {
+          label: 'No'
+        }
+      ]
+    });
+  };
+
+  return (
+    <StyledCenterContainer>
+      <h2>Titan Habit Tracker</h2>
+      <Timer
+        days={props.days}
+        hours={props.hours}
+        minutes={props.minutes}
+        seconds={props.seconds}
+      />
+      <StyledTimerButtonContainer>
+        <TimerButton text="スタート" handleClick={props.startTimer} />
+        <TimerButton text="リセット" handleClick={confirm} />
+      </StyledTimerButtonContainer>
+    </StyledCenterContainer>
+  );
+};
 
 export default App;
